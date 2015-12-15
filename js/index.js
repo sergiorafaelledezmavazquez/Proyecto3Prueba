896d6341b5125c57840cef53d3934bd0e2eea39f
sergio rafael ledezma vazquez <sergio_ledezma_vazquez@hotmail.com>
2015-12-15 13:03:13 -0600

    Probando phoneGap
    pruebo que funcione cambio de texto

--- js/index.js
+++ js/index.js
@@ -1,4 +1,14 @@
+
 document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
     alert('Funciono');
+    $("#resultado").html("PhoneGap esta funcionando")
 }
+/*
+function onBodyLoad(){
+    document.addEventListener("deviceready",onDeviceReady,false);
+}
+function onDeviceReady() {
+    $("#resultado").html("PhoneGap esta funcionando")
+}
+*/
