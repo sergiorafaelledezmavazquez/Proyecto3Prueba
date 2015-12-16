15b45a0ae0694d005d13115ca188ea7a4a69c052
sergio rafael ledezma vazquez <sergio_ledezma_vazquez@hotmail.com>
2015-12-16 09:50:23 -0600

    Probando obtener el archivo imagen
    cordova.file.applicationDirectory

--- js/index.js
+++ js/index.js
@@ -2,13 +2,26 @@
 document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
     alert('Funciono');
-    $("#resultado").html("PhoneGap esta funcionando")
+    $("#resultado").html("PhoneGap esta funcionando");
+    
+    
+    window.resolveLocalFileSystemURL(cordova.file.applicationDirectory + "res/imagenes/jquery-logo.png", gotFile, fail);
+    
+    
 }
-/*
-function onBodyLoad(){
-    document.addEventListener("deviceready",onDeviceReady,false);
+
+function gotFile(fileEntry){
+    fileEntry.file(function(file) {
+		var reader = new FileReader();
+		reader.onloadend = function(e) {
+			console.log("Text is: "+this.result);
+			//document.querySelector("#textArea").innerHTML = this.result;
+            $('#employeePic').attr('src', this.result);
+		}
+		reader.readAsText(file);
+	});
 }
-function onDeviceReady() {
-    $("#resultado").html("PhoneGap esta funcionando")
+function fail(e) {
+   console.log("FileSystem Error");
+   console.dir(e);
 }
-*/
