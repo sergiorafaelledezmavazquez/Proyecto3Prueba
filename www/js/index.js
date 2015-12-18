aa8b389ab948ae519f96f3353000784856f6d538
sergio rafael ledezma vazquez <sergio_ledezma_vazquez@hotmail.com>
2015-12-18 10:17:52 -0600

    Quite demos de JQUERY
    quite la carpeta demos de la carpeta Jquerymobile

--- www/js/index.js
+++ www/js/index.js
@@ -37,7 +37,7 @@ $el
 
 // Kick off one resize to fix all videos on page load
 }).resize();
-    
+
   //-------------------------------------------------
     //alert('Funciono');
     $("#resultado").html("PhoneGap esta funcionando");
