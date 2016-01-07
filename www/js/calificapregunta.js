67bb6d818ed019f1d2a56209399ba4c1c9f21489
sergio rafael ledezma vazquez <sergio_ledezma_vazquez@hotmail.com>
2016-01-07 14:02:47 -0600

    Ok
    OK

---
+++ www/js/calificapregunta.js
@@ -0,0 +1,14 @@
+/*
+var mostrarValor = function(x){
+	//alert("El valor: "+x.value+" y el texto: "+x.options[x.selectedIndex].text);
+    alert("El valor: "+x.value);
+}
+*/
+    $(document).ready(function(){
+        
+        $("#preg1-opcion-b").click(function(){
+            alert("Hola Sergio");
+        });
+        
+        
+    });
